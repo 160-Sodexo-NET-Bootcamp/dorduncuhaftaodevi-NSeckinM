b142c8156a345e0bd068bde4d899f939b67be942
NseckinM <seckinmantar@gmail.com>
2022-01-27 23:27:05 +0300

    Clean Architecture mold Applied & entity and Interfaces added

---
+++ NSeckinMantar_Odev4_BackgroundServices/Src/ApplicationCore/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        public string CustomerNumber { get; set; }
+
+        public DateTime CreationTime { get; set; } = DateTime.Now;
+
+        public bool Status { get; set; }
+
+
+
+    }
+}
---
+++ NSeckinMantar_Odev4_BackgroundServices/Src/ApplicationCore/Interfaces/UnitOfWork/IUnirOfWork.cs
@@ -0,0 +1,13 @@
+﻿using ApplicationCore.Interfaces.Repository;
+using System;
+
+namespace ApplicationCore.Interfaces.UnitOfWork
+{
+    public interface IUnirOfWork : IDisposable
+    {
+        IUserRepository Users { get; }
+
+        int Complete();
+
+    }
+}
